4747ed247b068050eb731ba00f63b8cd9c89d18d
zhengyi32 <zhengyi32@meituan.com>
2023-09-21 14:55:03 +0800

    add license

--- vector-stores/spring-ai-redis-store/src/main/java/org/springframework/ai/vectorstore/RedisVectorStore.java
+++ vector-stores/spring-ai-redis-store/src/main/java/org/springframework/ai/vectorstore/RedisVectorStore.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.ai.vectorstore;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -65,7 +81,12 @@ public class RedisVectorStore implements VectorStore, SmartLifecycle {
     @Override
     public void add(List<Document> documents) {
         Pipeline pipeline = jedisClient.pipelined();
-        documents.forEach(document -> pipeline.jsonSetWithEscape(prefix + ":" + document.getId(), document));
+        documents.stream()
+                .map(document -> {
+                    document.setEmbedding(embeddingClient.embed(document.getText()));
+                    return document;
+                })
+                .forEach(document -> pipeline.jsonSetWithEscape(prefix + ":" + document.getId(), document));
         pipeline.close();
     }
 
